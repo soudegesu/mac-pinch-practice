4557196d6114a93e0c758cc2af5d3b9fe420d4a5
soudegesu <soudegesu@hotmail.co.jp>
2021-03-25 17:11:08 +0900

    initial commit

---
+++ mac-pinch-practiceTests/mac_pinch_practiceTests.swift
@@ -0,0 +1,33 @@
+//
+//  mac_pinch_practiceTests.swift
+//  mac-pinch-practiceTests
+//
+//  Created by Takaaki Suzuki on 2021/03/25.
+//
+
+import XCTest
+@testable import mac_pinch_practice
+
+class mac_pinch_practiceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

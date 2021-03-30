bc3327376908f966a0f233ce8804657b507abfa4
soudegesu <soudegesu@hotmail.co.jp>
2021-03-30 16:18:53 +0900

    reset previous magnificant

--- mac-pinch-practice/PinchZoomView.swift
+++ mac-pinch-practice/PinchZoomView.swift
@@ -17,13 +17,7 @@ class PinchZoomView: NSView {
         delegate?.pinchZoomView(self, didChangeScale: scale)
       }
   }
-  
-  private(set) var minScale: CGFloat = 1.0 {
-    didSet {
-        delegate?.pinchZoomView(self, didChangeMinScale: minScale)
-    }
-  }
-
+    
   private(set) var anchor: UnitPoint = .center {
       didSet {
           delegate?.pinchZoomView(self, didChangeAnchor: anchor)
@@ -39,6 +33,7 @@ class PinchZoomView: NSView {
   private var startLocation: CGPoint = .zero
   private var location: CGPoint = .zero
   private var previousMagnification: CGFloat = 0.0
+  private let minScale: CGFloat = 1.0
 
   init() {
       super.init(frame: .zero)
@@ -52,25 +47,25 @@ class PinchZoomView: NSView {
   }
   
   @objc private func pinch(gesture: NSMagnificationGestureRecognizer) {
-      let magnification = gesture.magnification
-      let currentMagnification = previousMagnification + magnification
-      let scaleFactor = (currentMagnification >= 0.0) ? (1.0 + currentMagnification) : 1.0 / (1.0 - currentMagnification)
-      location = gesture.location(in: self)
-      
+      let currentMagnification = previousMagnification + gesture.magnification
+    
       switch gesture.state {
       case .began:
         isPinching = true
         startLocation = location
         anchor = UnitPoint(x: startLocation.x / bounds.width, y: (bounds.height - startLocation.y)  / bounds.height)
-        debugPrint(location)
       case .changed:
+        let scaleFactor = (currentMagnification >= 0.0) ? (1.0 + currentMagnification) : 1.0 / (1.0 - currentMagnification)
+        location = gesture.location(in: self)
         scale = scaleFactor
       case .ended, .cancelled, .failed:
         isPinching = false
         if scale <= minScale {
           scale = minScale
+          previousMagnification = 0.0
+        } else {
+          previousMagnification = currentMagnification
         }
-        previousMagnification = currentMagnification
       default:
           break
       }
@@ -80,7 +75,6 @@ class PinchZoomView: NSView {
 protocol PinchZoomViewDelgate: AnyObject {
   func pinchZoomView(_ pinchZoomView: PinchZoomView, didChangePinching isPinching: Bool)
   func pinchZoomView(_ pinchZoomView: PinchZoomView, didChangeScale scale: CGFloat)
-  func pinchZoomView(_ pinchZoomView: PinchZoomView, didChangeMinScale minScale: CGFloat)
   func pinchZoomView(_ pinchZoomView: PinchZoomView, didChangeAnchor anchor: UnitPoint)
 }
 
@@ -89,7 +83,6 @@ struct PinchZoom: NSViewRepresentable {
   typealias NSViewType = PinchZoomView
   
   @Binding var scale: CGFloat
-  @Binding var minScale: CGFloat
   @Binding var anchor: UnitPoint
   @Binding var isPinching: Bool
   
@@ -107,7 +100,7 @@ struct PinchZoom: NSViewRepresentable {
   }
   
   class Coordinator: NSObject, PinchZoomViewDelgate {
-    var pinchZoom: PinchZoom
+    private var pinchZoom: PinchZoom
 
     init(_ pinchZoom: PinchZoom) {
         self.pinchZoom = pinchZoom
@@ -120,10 +113,6 @@ struct PinchZoom: NSViewRepresentable {
     func pinchZoomView(_ pinchZoomView: PinchZoomView, didChangeScale scale: CGFloat) {
       pinchZoom.scale = scale
     }
-
-    func pinchZoomView(_ pinchZoomView: PinchZoomView, didChangeMinScale minScale: CGFloat) {
-      pinchZoom.minScale = minScale
-    }
     
     func pinchZoomView(_ pinchZoomView: PinchZoomView, didChangeAnchor anchor: UnitPoint) {
         pinchZoom.anchor = anchor
@@ -131,26 +120,25 @@ struct PinchZoom: NSViewRepresentable {
   }
 }
 
-struct PinchToZoom: ViewModifier {
+struct PinchToZoomModifier: ViewModifier {
   
   @State var scale: CGFloat = 1.0
-  @State var minScale: CGFloat = 1.0
   @State var anchor: UnitPoint = .center
   @State var isPinching: Bool = false
-  var width: CGFloat
-  var height: CGFloat
+  var width: CGFloat = 0.0
+  var height: CGFloat = 0.0
   
   func body(content: Content) -> some View {
     content
       .aspectRatio(contentMode: .fit)
       .frame(width: width * scale, height: height * scale, alignment: .center)
       .animation(isPinching ? .none : .spring())
-      .overlay(PinchZoom(scale: $scale, minScale: $minScale, anchor: $anchor, isPinching: $isPinching))
+      .overlay(PinchZoom(scale: $scale, anchor: $anchor, isPinching: $isPinching))
   }
 }
 
 extension View {
-  func  pinchToZoom(width: CGFloat, height: CGFloat) -> some View {
-    self.modifier(PinchToZoom(width: width, height: height))
+  func pinchToZoom(width: CGFloat, height: CGFloat) -> some View {
+    self.modifier(PinchToZoomModifier(width: width, height: height))
   }
 }
